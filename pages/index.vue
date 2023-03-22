6bbb6f85dcc6558240f929bce988dbfe3bd6aa11
martoast <alexmartos96@gmail.com>
2023-03-21 21:06:31 -0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <pre>
+            {{data}}
+        </pre>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+const { data } = await useFetch(() => "http://api.laragigs.test/users")
+
+</script>
