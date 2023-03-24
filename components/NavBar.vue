32a661aa1440528b61914c8add314d731cca1153
martoast <alexmartos96@gmail.com>
2023-03-23 19:13:29 -0700

    listings page

---
+++ components/NavBar.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Laragigs</a>
+            <div v-if="user" class="dropstart">
+                <button class="btn btn-secondary avatar" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                    </svg>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                    <li><a class="dropdown-item" :href="'/user/' + user.uuid">Profile</a></li>
+                    <li>
+                        <button class="dropdown-item" type="button" @click="$logout">
+                            Logout
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+const user = readonly(useUser());
+
+const { $logout } = useNuxtApp();
+
+</script>
+
+<style scoped>
+.avatar {
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+}
+</style>
--- pages/listings/index.vue
+++ pages/listings/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="container my-4">
-        <h3 class="pb-3">Listings:</h3>
+        <h3 class="pb-4">Listings:</h3>
        <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
         <div class="col" v-for="listing in listings" :key="listing.id">
-            <div class="card h-100" style="width: 18rem;">
+            <div class="card h-100">
             <div class="card-body">
-                <h5 class="card-title">{{listing.title}}</h5>
+                <a :href="'/listings/' + listing.id" style="text-decoration: none;"><h5 class="card-title">{{listing.title}}</h5></a>
+                <p class="card-text">Salary: {{listing.salary}}</p>
                 <p class="card-text">{{listing.description}}</p>
-                <a :href="'/listings/' + listing.id" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
 
